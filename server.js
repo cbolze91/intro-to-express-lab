1668a89484301d4421f4aa9ec71de30eed3664c4
Christina <christinamarieelaine@gmail.com>
2026-02-02 11:16:42 -0800

    exercise 1

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const app = express()
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
+
+// Exercise 1
+
+app.get('/greetings/:username', (req, res) => {
+    res.send(`Hello there, ${req.params.username}!`)
+})
+
