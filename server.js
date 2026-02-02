9b6e28a9e1814a8fe0a63492338b4846350ccd25
Christina <christinamarieelaine@gmail.com>
2026-02-02 13:59:40 -0800

    exercsie 3

--- server.js
+++ server.js
@@ -28,3 +28,25 @@ app.get('/roll/:number', (req,res) => {
 
 // Exercise 2 URL test http://loclhost:3000/roll/20
 //  http://loclhost:3000/roll/potato
+
+// Exrcise 3: I Want THAT One!
+
+  const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+  app.get('/collectibles/:index', (req, res) => {
+    const index = Number(req.params.index)
+    const item = collectibles[index]
+
+    if (!item) {
+        return res.send ("This item is not yet in stock. Check back soon!")
+    }    
+
+    res.send( `So, you want the ${item.name}? For $${item.price}, it can be yours!`)
+  })
+
+  // Exercise 3 URL test http://localhost:3000/collectibles/0
+  //  http://localhost:3000/collectibles/99
